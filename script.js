d05d65a5352565520ae814dbdc004901a545e205
Sonia Pires <157916578+soniathephoenix@users.noreply.github.com>
2024-12-05 10:14:08 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const days = document.querySelectorAll('.day')
+const messageBox = document.getElementById('messageBox')
+days.forEach(day => {
+    day.addEventListener('click', () => {
+        const message = day.getAttribute('data-message');
+        messageBox.textContent = message;
+        messageBox.style.visibility = 'visible'
+    });
+})
